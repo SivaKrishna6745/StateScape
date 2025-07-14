4be08c1e4158f51f14b18c3964886ba11ce50598
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-14 12:06:51 +0530

    feat: Install @reduxjs package for store setup feat: Creat useStateObserver hook for slice monitoring

---
+++ src/core/useStoreObserver.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+import type { Store } from '@reduxjs/toolkit';
+
+type StateChangeCallback = (slice: string, prevValue: any, nextValue: any) => void;
+const useStateObserver = (store: Store, onChange: StateChangeCallback) => {
+    const prevStateRef = useRef(store.getState());
+    useEffect(() => {
+        const unsubscribe = store.subscribe(() => {
+            const nextState = store.getState();
+            const prevState = prevStateRef.current;
+            Object.keys(nextState).forEach((sliceKey) => {
+                if (prevState[sliceKey] !== nextState[sliceKey]) {
+                    onChange(sliceKey, prevState[sliceKey], nextState[sliceKey]);
+                }
+            });
+            prevStateRef.current = nextState;
+        });
+        return () => unsubscribe();
+    }, [store, onChange]);
+};
+
+export default useStateObserver;
